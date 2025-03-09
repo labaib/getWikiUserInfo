73af152b21767b8c565cb23b792a9e0ff3faca70
logo94 <lorenzo.gobbo@hotmail.it>
2025-03-09 14:41:34 +0100

    error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const getWikiUserInfo = async (endpoint) => {
     });
 
     try {
-        const url = `https://${apiEndpoint}/w/api.php?${params.toString()}`
+        const url = `https://${endpoint}/w/api.php?${params.toString()}`
         const response = await fetch(url, { credentials: "include" });
         const data = await response.json();
 
